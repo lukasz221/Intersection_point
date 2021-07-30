f048470e09a4f55f6d3507cc275d78ebc8279e5a
Łukasz Zalewski <lukaszzalewski1820@wp.pl>
2021-07-30 17:11:10 +0200

    Add project files.

---
+++ Intersection point/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Intersection_point
+{
+    class Program
+    {
+        public static List<int> a = new List<int>();
+        public static List<int> b = new List<int>();
+
+
+        static void Main(string[] args)
+        {
+            FillList();
+
+            Console.WriteLine(Point().ToString());
+
+            Console.ReadKey();
+        }
+
+        public static int Point()
+        {
+            bool check = false;
+
+            foreach (var item in a)
+            {
+                check = b.Contains(item);
+                if (check)
+                {
+                    return item;
+                }
+            }
+
+            return 0;
+        }
+
+        public static void FillList()
+        {
+            a.Add(3);
+            a.Add(7);
+            a.Add(8);
+            a.Add(10);
+
+            b.Add(99);
+            b.Add(1);
+            b.Add(8);
+            b.Add(10);
+        }
+    }
+}
